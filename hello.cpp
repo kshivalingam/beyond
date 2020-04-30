890b4b7a3c5d8e9f24f48b6e06f04f99aa8b378b
Your Name <you@example.com>
2020-04-30 21:01:07 +0530

    commands

--- hello.cpp
+++ hello.cpp
@@ -5,4 +5,8 @@ int main(){
     cout<<"first repository"<<endl;
     cout<<"change is made"<<endl;
     cout<<"changes made without push"<<endl;
+    cout<<"git clone https llink"<<endl;
+    cout<<"git add"<<endl;
+    cout<<" git commit -m "" "<<endl;
+    cout<<"git push"<<endl;
 }
